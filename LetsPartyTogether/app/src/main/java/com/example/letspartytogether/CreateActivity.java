578bb5763b533a9252ebc8f1be41b2dc54434160
Giulio Mondo <giulio.mondo@gmail.com>
2020-04-30 19:14:32 +0200

    ..........

--- LetsPartyTogether/app/src/main/java/com/example/letspartytogether/CreateActivity.java
+++ LetsPartyTogether/app/src/main/java/com/example/letspartytogether/CreateActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class CreateActivity extends AppCompatActivity {
 
     private EditText etPartyName, etPartyHost;
     private Button bttCreate2;
+    private static final String fileName = "PartyList.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
