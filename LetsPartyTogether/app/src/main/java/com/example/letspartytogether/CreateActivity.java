ab520b96295bcf1ccb9384b2473094e97a65afa4
Giulio Mondo <giulio.mondo@gmail.com>
2020-04-30 16:56:15 +0200

    ....

--- LetsPartyTogether/app/src/main/java/com/example/letspartytogether/CreateActivity.java
+++ LetsPartyTogether/app/src/main/java/com/example/letspartytogether/CreateActivity.java
@@ -2,6 +2,7 @@ package com.example.letspartytogether;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,12 @@ public class CreateActivity extends AppCompatActivity {
         bttCreate2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String _nomeParty = etPartyName.getText().toString();
+
+                Intent intentToPartyActivity = new Intent("android.intent.action.PartyActivity");
+                intentToPartyActivity.putExtra(getString(R.string.STRINGA_CreateToParty), _nomeParty);
+                startActivity(intentToPartyActivity);
 
             }
         });
--- LetsPartyTogether/app/src/main/java/com/example/letspartytogether/PartyActivity.java
+++ LetsPartyTogether/app/src/main/java/com/example/letspartytogether/PartyActivity.java
@@ -2,6 +2,10 @@ package com.example.letspartytogether;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +13,7 @@ import android.widget.TextView;
 
 public class PartyActivity extends AppCompatActivity {
 
+    private TextView tvFinalPartyCode;
     private TextView tvPartyName, tvPartyCode;
     private Button bttCopyPartyCode;
 
@@ -18,13 +23,22 @@ public class PartyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party);
 
+        tvFinalPartyCode = findViewById(R.id.tvFinalPartyCode);
         tvPartyName = findViewById(R.id.tvPartyName);
         tvPartyCode = findViewById(R.id.tvPartyCode);
         bttCopyPartyCode = findViewById(R.id.bttCopyPartyCode);
 
+        Intent partyIntent = getIntent();
+        String title = partyIntent.getStringExtra(getString(R.string.STRINGA_CreateToParty));
+        tvPartyName.setText(title);
+
         bttCopyPartyCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Code: ", tvPartyCode.getText().toString());
+                clipboard.setPrimaryClip(clip);
 
             }
         });
