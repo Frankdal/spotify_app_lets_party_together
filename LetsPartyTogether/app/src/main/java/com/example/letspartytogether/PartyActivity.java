82c7490fa832c1cfc7a6b60c9c10862bb9e9041d
Lorenzo Salvemini <salveminilorenzo@live.it>
2020-05-06 11:00:53 +0200

    Revert ".........."
    This reverts commit 578bb5763b533a9252ebc8f1be41b2dc54434160.

--- LetsPartyTogether/app/src/main/java/com/example/letspartytogether/PartyActivity.java
+++ LetsPartyTogether/app/src/main/java/com/example/letspartytogether/PartyActivity.java
@@ -15,7 +15,7 @@ public class PartyActivity extends AppCompatActivity {
 
     private TextView tvFinalPartyCode;
     private TextView tvPartyName, tvPartyCode;
-    private Button bttCopyPartyCode, bttAddSong;
+    private Button bttCopyPartyCode;
 
 
     @Override
@@ -27,7 +27,6 @@ public class PartyActivity extends AppCompatActivity {
         tvPartyName = findViewById(R.id.tvPartyName);
         tvPartyCode = findViewById(R.id.tvPartyCode);
         bttCopyPartyCode = findViewById(R.id.bttCopyPartyCode);
-        bttAddSong = findViewById(R.id.bttAddSong);
 
         Intent partyIntent = getIntent();
         String title = partyIntent.getStringExtra(getString(R.string.STRINGA_CreateToParty));
@@ -41,15 +40,6 @@ public class PartyActivity extends AppCompatActivity {
                 ClipData clip = ClipData.newPlainText("Code: ", tvPartyCode.getText().toString());
                 clipboard.setPrimaryClip(clip);
 
-            }
-        });
-
-
-        bttAddSong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentAddSongToResearch = new Intent("android.intent.action.ResearchActivity");
-                startActivity(intentAddSongToResearch);
             }
         });
 
